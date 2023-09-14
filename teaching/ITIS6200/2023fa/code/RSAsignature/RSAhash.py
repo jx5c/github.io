1cb6e3faa87c0a63a852bd53b786ce1b3b686a5a
Jian Xiang <jx5c@virginia.edu>
2023-09-14 13:37:56 -0400

    checkpoint

--- teaching/ITIS6200/2023fa/code/RSAsignature/RSAhash.py
+++ teaching/ITIS6200/2023fa/code/RSAsignature/RSAhash.py
@@ -4,4 +4,4 @@ msg = (input("Enter a message to hash: ")).encode()
 
 from hashlib import sha512
 hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
-print("Signature:", hex(hash))
+print("HASH:", hex(hash))
